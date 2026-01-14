aede7dd4846f4bf4ba36313c3a7a826f570bba64
Pierpaolo Calanna <p.calanna@gmail.com>
2026-01-14 13:23:23 +0100

    Update m__embeddings.py

--- m__embeddings.py
+++ m__embeddings.py
@@ -6,28 +6,43 @@ app = marimo.App(width="full")
 
 @app.cell
 def _():
+    from pathlib import Path
+
     import pandas as pd
     import numpy as np
     from lib.embeddings import get_batch_embeddings
-    return get_batch_embeddings, pd
+    return Path, get_batch_embeddings, np, pd
 
 
 @app.cell
 def _(pd):
-    texts = pd.read_csv("./data/psycarticles_cleaned.csv")
-    texts.head()
-    return (texts,)
+    EMBEDDING_MODEL_NAME = "text-embedding-3-large"
+    df = pd.read_csv("./data/psycarticles_cleaned.csv")
+    df.shape
+    return EMBEDDING_MODEL_NAME, df
 
 
 @app.cell
-def _(get_batch_embeddings, texts):
-    m = get_batch_embeddings(texts.iloc[:2,-1].to_list())
-    return (m,)
+def _(EMBEDDING_MODEL_NAME, df, get_batch_embeddings):
+    texts_to_embed = df.text_to_embed.to_list()
+    _, embeddings = get_batch_embeddings(texts_to_embed, embedding_model_name=EMBEDDING_MODEL_NAME)
+    return (embeddings,)
 
 
 @app.cell
-def _(m):
-    m
+def _(EMBEDDING_MODEL_NAME, Path, embeddings, np):
+    embedding_model_name_filepath = Path("./data/embeddings/embedding_model_name.txt")
+    with embedding_model_name_filepath.open("w") as f:
+        f.write(EMBEDDING_MODEL_NAME)
+
+    embeddings_filepath = Path("./data/embeddings/embeddings.npy")
+    np.save(embeddings_filepath, np.array(embeddings))
+    return
+
+
+@app.cell
+def _(embeddings, np):
+    np.array(embeddings).shape
     return
 
 
