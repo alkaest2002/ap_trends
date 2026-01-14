1919df0ac387587b159a570ccc86e6ffce91ac42
Pierpaolo Calanna <p.calanna@gmail.com>
2026-01-14 15:29:27 +0100

    Create m__bertopic_analysis.py

---
+++ m__bertopic_analysis.py
@@ -0,0 +1,120 @@
+import marimo
+
+__generated_with = "0.19.2"
+app = marimo.App(width="full")
+
+
+@app.cell
+def _():
+    from pathlib import Path
+
+    import numpy as np
+    import pandas as pd
+    from umap import UMAP
+    from hdbscan import HDBSCAN
+    from sklearn.feature_extraction.text import CountVectorizer
+
+    from bertopic import BERTopic
+    from bertopic.representation import KeyBERTInspired
+    from bertopic.vectorizers import ClassTfidfTransformer
+    return (
+        BERTopic,
+        ClassTfidfTransformer,
+        CountVectorizer,
+        HDBSCAN,
+        KeyBERTInspired,
+        Path,
+        UMAP,
+        np,
+        pd,
+    )
+
+
+@app.cell
+def _(pd):
+    df = pd.read_csv("./data/psycarticles_cleaned.csv")
+    df.head()
+    return (df,)
+
+
+@app.cell
+def _(df):
+    docs = df.doc.to_list()
+    docs[0]
+    return (docs,)
+
+
+@app.cell
+def _(Path):
+    embedding_model_name = ""
+    with Path("./data/embeddings/embedding_model_name.txt").open("r") as f:
+        embedding_model_name = f.read()
+    embedding_model_name
+    return
+
+
+@app.cell
+def _(Path, np):
+    embeddings = np.load(Path("./data/embeddings/embeddings.npy"))
+    embeddings.shape
+    return (embeddings,)
+
+
+@app.cell
+def _(
+    BERTopic,
+    ClassTfidfTransformer,
+    CountVectorizer,
+    HDBSCAN,
+    KeyBERTInspired,
+    UMAP,
+):
+    # Step 1 - Extract embeddings
+    embedding_model = None
+
+    # Step 2 - Reduce dimensionality
+    umap_model = UMAP(n_neighbors=15, n_components=30, min_dist=0.0, metric='cosine')
+
+    # Step 3 - Cluster reduced embeddings
+    hdbscan_model = HDBSCAN(min_cluster_size=15, metric='euclidean', cluster_selection_method='eom', prediction_data=True)
+
+    # Step 4 - Tokenize topics
+    vectorizer_model = CountVectorizer(stop_words="english")
+
+    # Step 5 - Create topic representation
+    ctfidf_model = ClassTfidfTransformer()
+
+    # Step 6 - (Optional) Fine-tune topic representations with
+    # a `bertopic.representation` model
+    representation_model = KeyBERTInspired()
+
+    # All steps together
+    topic_model = BERTopic(
+      embedding_model=embedding_model,          # Step 1 - Extract embeddings
+      umap_model=umap_model,                    # Step 2 - Reduce dimensionality
+      hdbscan_model=hdbscan_model,              # Step 3 - Cluster reduced embeddings
+      vectorizer_model=vectorizer_model,        # Step 4 - Tokenize topics
+      ctfidf_model=ctfidf_model,                # Step 5 - Extract topic words
+    )
+    return (topic_model,)
+
+
+@app.cell
+def _(docs, embeddings, topic_model):
+    topics, probs = topic_model.fit_transform(docs, embeddings=embeddings)
+    return
+
+
+@app.cell
+def _(topic_model):
+    topic_model.get_topic_info()
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
